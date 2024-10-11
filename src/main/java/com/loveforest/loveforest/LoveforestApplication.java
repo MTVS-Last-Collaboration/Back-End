da3b6a74ce8344a0f5abcb050561463e0c506c62
YachaTree <dnftksghkfkd@icloud.com>
2024-10-11 10:00:54 +0900

    project setting

---
+++ src/main/java/com/loveforest/loveforest/LoveforestApplication.java
@@ -0,0 +1,13 @@
+package com.loveforest.loveforest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveforestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveforestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loveforest/loveforest/LoveforestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loveforest.loveforest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveforestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

be39e02953f66e5deb67698d8e7c55bd92b06be6
YachaTree <dnftksghkfkd@icloud.com>
2024-11-05 01:57:16 +0900

    Feat: 일일 토픽 예외처리 추가

--- src/main/java/com/loveforest/loveforest/domain/boardpost/service/DailyTopicService.java
+++ src/main/java/com/loveforest/loveforest/domain/boardpost/service/DailyTopicService.java
@@ -1,6 +1,7 @@
 package com.loveforest.loveforest.domain.boardpost.service;
 
 import com.loveforest.loveforest.domain.boardpost.entity.DailyTopic;
+import com.loveforest.loveforest.domain.boardpost.exception.DailyTopicNotFoundException;
 import com.loveforest.loveforest.domain.boardpost.repository.DailyTopicRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,8 +20,9 @@ public class DailyTopicService {
         return dailyTopicRepository.save(dailyTopic);
     }
 
-    public Optional<DailyTopic> getDailyTopicByDate(LocalDate date) {
-        return dailyTopicRepository.findByDate(date);
+    public DailyTopic getDailyTopicByDate(LocalDate date) {
+        return dailyTopicRepository.findByDate(date)
+                .orElseThrow(DailyTopicNotFoundException::new); // 예외 추가
     }
 
     public Optional<DailyTopic> getDailyTopicById(Long dailyTopicId) {

6445ec25d642ec9b5c5b921c3efbc7c340e38539
YachaTree <dnftksghkfkd@icloud.com>
2024-11-07 17:27:58 +0900

    Feat: 게시글 및 댓글 responseDTO에 좋아요 수 추가

--- src/main/java/com/loveforest/loveforest/domain/boardpost/dto/CommentResponseDTO.java
+++ src/main/java/com/loveforest/loveforest/domain/boardpost/dto/CommentResponseDTO.java
@@ -25,6 +25,9 @@ public class CommentResponseDTO {
     @Schema(description = "작성자 닉네임", example = "user123")
     private String authorNickname;
 
+    @Schema(description = "댓글 좋아요 수", example = "1")
+    private int likeCount;
+
     @Schema(description = "댓글 생성일", example = "2023-01-01T12:00:00")
     private LocalDateTime createdDate;
 }

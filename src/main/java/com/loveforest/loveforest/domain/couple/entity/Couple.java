ed432ae49aa9ef5119a0b0f57035109dd597169a
YachaTree <dnftksghkfkd@icloud.com>
2024-11-07 18:09:48 +0900

    Fix: 커플 포인트 flower -> couple entity로 수정

--- src/main/java/com/loveforest/loveforest/domain/couple/entity/Couple.java
+++ src/main/java/com/loveforest/loveforest/domain/couple/entity/Couple.java
@@ -23,6 +23,10 @@ public class Couple extends BaseTimeEntity {
     @Column(name = "couple_code", nullable = false, unique = true)
     private String coupleCode;
 
+    @Column(nullable = false)
+    private int points = 0;  // 커플 포인트 필드 추가
+
+
     // 사용자들 (1:N 관계로 한 커플에 2명의 사용자 연결)
     @OneToMany(mappedBy = "couple", cascade = {CascadeType.MERGE, CascadeType.PERSIST}, orphanRemoval = false)
     private List<User> users = new ArrayList<>();
@@ -37,5 +41,9 @@ public class Couple extends BaseTimeEntity {
             users.add(user);
             user.setCouple(this);  // 사용자와 커플 간의 관계 설정
         }
+    }
+
+    public void addPoints(int points) {
+        this.points += points;
     }
 }

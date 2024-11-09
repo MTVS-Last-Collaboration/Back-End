02088bf9e0f313115eaaab7d9d755eee1b421e4a
YachaTree <dnftksghkfkd@icloud.com>
2024-11-09 23:58:47 +0900

    Fix: Furniture type 필드 삭제

--- src/main/java/com/loveforest/loveforest/domain/room/repository/WallpaperRepository.java
+++ src/main/java/com/loveforest/loveforest/domain/room/repository/WallpaperRepository.java
@@ -4,11 +4,7 @@ import com.loveforest.loveforest.domain.room.entity.Wallpaper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface WallpaperRepository extends JpaRepository<Wallpaper, Long> {
-    Optional<Wallpaper> findByWallpaperNumber(int wallpaperNumber);
-    List<Wallpaper> findAllByOrderByWallpaperNumberAsc();
 }

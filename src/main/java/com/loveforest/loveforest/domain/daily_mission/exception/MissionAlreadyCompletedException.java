3ee3b0ff3d8f5ebbb6969e105bd928286e4b7bb8
YachaTree <dnftksghkfkd@icloud.com>
2024-11-12 15:16:34 +0900

    Feat: 답변 complete되고 수정 가능하도록 수정(임시)

--- src/main/java/com/loveforest/loveforest/domain/daily_mission/exception/MissionAlreadyCompletedException.java
+++ src/main/java/com/loveforest/loveforest/domain/daily_mission/exception/MissionAlreadyCompletedException.java
@@ -4,7 +4,7 @@ import com.loveforest.loveforest.exception.CustomException;
 import com.loveforest.loveforest.exception.ErrorCode;
 
 public class MissionAlreadyCompletedException extends CustomException {
-    public MissionAlreadyCompletedException {
+    public MissionAlreadyCompletedException() {
         super(ErrorCode.MISSION_ALREADY_COMPLETED); // ErrorCode에 추가 필요
     }
 }

cf9ed9edb5841e81992d947f84dca4b559e98d08
YachaTree <dnftksghkfkd@icloud.com>
2024-11-23 23:10:50 +0900

    Feat: login responseDto 필드 추가

--- src/main/java/com/loveforest/loveforest/domain/user/dto/LoginResponseDTO.java
+++ src/main/java/com/loveforest/loveforest/domain/user/dto/LoginResponseDTO.java
@@ -1,12 +1,17 @@
 package com.loveforest.loveforest.domain.user.dto;
 
+import com.loveforest.loveforest.domain.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginResponseDTO {
     private String coupleCode;
+    private String myNickname;
+    private String partnerNickname;
 }

9c7b12f5c9e91c47d62f6eb304de7b111144edde
YachaTree <dnftksghkfkd@icloud.com>
2024-12-09 17:53:50 +0900

    Feat: JacksonConfig 추가

--- src/main/java/com/loveforest/loveforest/config/JacksonConfig.java
+++ src/main/java/com/loveforest/loveforest/config/JacksonConfig.java
@@ -1,6 +1,8 @@
 package com.loveforest.loveforest.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,8 @@ public class JacksonConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule()); // JavaTimeModule 등록
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false); // 날짜를 문자열로 직렬화
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); // 알 수 없는 필드 무시
         return objectMapper;
     }
 }

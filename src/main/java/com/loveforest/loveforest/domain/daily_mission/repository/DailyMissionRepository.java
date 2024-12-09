c89524813c549e79de70b8d00e7698d8caa14a2a
YachaTree <dnftksghkfkd@icloud.com>
2024-12-09 14:59:54 +0900

    Fix: 데이터 무결성이 보장

--- src/main/java/com/loveforest/loveforest/domain/daily_mission/repository/DailyMissionRepository.java
+++ src/main/java/com/loveforest/loveforest/domain/daily_mission/repository/DailyMissionRepository.java
@@ -11,5 +11,8 @@ public interface DailyMissionRepository extends JpaRepository<DailyMission, Long
     Optional<DailyMission> findByCouple_IdAndMissionDate(Long coupleId, LocalDate date);
     List<DailyMission> findByCouple_IdOrderByMissionDateDesc(Long coupleId);
     boolean existsByMissionDate(LocalDate date);
+
+    // 존재 여부 확인 메서드 추가
+    boolean existsByCouple_IdAndMissionDate(Long coupleId, LocalDate date);
 }
 
